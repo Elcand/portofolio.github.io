63ee9cb71e3e7d38467c0e68ed8ab63f79c862fc
Mikhael Candra <tjandraraharja99@gmail.com>
2025-12-25 12:10:20 +0700

    feat: make about section on home page

---
+++ src/components/pages/home/section/introsection.tsx
@@ -0,0 +1,36 @@
+function Intro() {
+  return (
+    <>
+      <section id="about" className="py-20 bg-gray-100">
+        <div className="container px-6 mx-auto">
+          <h2 className="mb-12 text-4xl font-bold text-center">About Me</h2>
+          <div className="max-w-6xl mx-auto text-left">
+            <p className="mb-6 text-lg leading-relaxed text-gray-700">
+              I am a vocational high school student majoring in Software
+              Engineering who underwent a Field Work Internship (PKL) at a
+              software house as a Full-Stack Developer. During my internship, I
+              was involved in developing web-based applications and had the
+              opportunity to handle both the backend and frontend development
+              processes.
+            </p>
+            <p className="mb-6 text-lg leading-relaxed text-gray-700">
+              During my internship, I worked on various application features
+              such as data management, user authentication, frontend-backend
+              integration, and developing functional and responsive interfaces.
+              This experience helped me understand the professional software
+              development workflow, including the use of version control and the
+              implementation of a clean and organized code structure.
+            </p>
+            <p className="text-lg leading-relaxed text-gray-700">
+              The programming languages ​​and technologies I have used include 
+              <strong> PHP (Laravel)</strong>, <strong>JavaScript (Vanilla, Node.js, React.js, Next.js,
+              Astro)</strong>, <strong> HTML</strong>, <strong>CSS</strong>, and <strong>SQL (MySQL)</strong> .
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Intro;
---
+++ src/components/pages/home/view/home.tsx
@@ -0,0 +1,13 @@
+import HeroSection from "../section/herosection";
+import IntroSection from "../section/introsection";
+
+function Home() {
+  return (
+    <>
+    <HeroSection />
+    <IntroSection />
+    </>
+  );
+}
+
+export default Home;
