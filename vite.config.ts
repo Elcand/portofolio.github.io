c0e0ebd203c5fcb3d12cd10173af525c3aab9725
Mikhael Candra <tjandraraharja99@gmail.com>
2026-01-06 15:07:51 +0700

    fix: set up deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portofolio.github.io/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
